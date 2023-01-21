92e2fe268b96b598d88c4f4df40f0a1aef5110cf
Leticia-C <leticia.camposg93@gmail.com>
2023-01-21 20:55:50 -0300

    modifica Dockerfile de front end

--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -22,20 +22,27 @@ describe('Testa Login', () => {
   .resolves(login);
    after(() => {(Users as sinon.SinonStub).restore()})
 
-  it('Testa se a requisisão POST retorna um status 201', async () => {
+  it('Testa se a requisisão POST na rota "/login" retorna um token e um status 200 se bem sucedida', async () => {
     await chai.request(app).post('/login').send(login)
     .end((_req, res) => {
     expect(res.status).to.be.equal(200);
     expect(res.body).to.deep.equal(token);
       })
     })
-  it('Erro ao tentar login sem um dos campos', () => {
-    chai.request(app).post('/login').send({ password: "secret_user" })
-    .end((_req, res) => {
-    expect(res.status).to.be.equal(400);
-    expect(res.body).to.deep.equal({ message: 'All fields must be filled' });
-      });
-   });
-   });
+    it('Se o login não tiver o campo "password", o resultado retornado deverá senviar uma mensagem de erro, com um status http 400', () => {
+        chai.request(app).post('/login').send({ email: "user@gmail.com" })
+        .end((_req, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res.body).to.deep.equal({ message: 'All fields must be filled' });
+          });
+       });
+    it('Se o login não tiver o campo "email", o resultado retornado deverá enviar uma mensagem de erro, com um status http 400:', () => {
+        chai.request(app).post('/login').send({ password: "secret_user" })
+        .end((_req, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res.body).to.deep.equal({ message: 'All fields must be filled' });
+        });
+    });
+    });
 });
 });
